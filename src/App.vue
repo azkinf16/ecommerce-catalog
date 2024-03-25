56018332cb0263ded0b3c362bfa5947b5fd8cc62
azkinf16 <azkifajri16@gmail.com>
2024-03-25 11:16:24 +0700

    add data fakestore api and slicing UI

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ import ProductDisplay from "./components/ProductDisplay.vue";
 .box {
   background: white;
   width: 100%;
-  max-width: 800px;
+  max-width: 950px;
   padding: 20px;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,33 +1,90 @@
 <template>
-  <!-- This is value of container  -->
-  <div class="container">
-    <!-- This is left secion of container -->
-    <div class="left-section">
-      <h1>ini gambar</h1>
+  <div :class="productCategory">
+
+    <!-- Loader -->
+    <div v-if="loading" class="loader"></div>
+    
+    <div
+      class="container"
+      v-if="productCategory === 'men-section' || productCategory === 'women-section'"
+    >
+      
+      <div class="left-section">
+        <img :src="product.image" alt="Product Image" />
+      </div>
+
+      
+      <div class="right-section">
+        <h1 id="title-product">{{ product.title }}</h1>
+        <div class="type-section">
+          <p id="type-product">{{ product.category }}</p>
+          <p>{{ product.rating?.rate }} ⭐</p>
+        </div>
+        <div class="divider"></div>
+        <p id="desc-product">{{ product.description }}</p>
+        <div class="divider"></div>
+        <h1>${{ product.price }}</h1>
+        <div class="button">
+          <div id="button-1">Buy Now</div>
+          <div id="button-2" @click="getNextProduct">Next Product</div>
+        </div>
+      </div>
     </div>
 
-    <!-- This is right secion of container -->
-    <div class="right-section">
-      <h1 id="title-product">Title</h1>
-      <div class="type-section">
-        <p id="type-product">men's clothing</p>
-        <p>2.9/5</p>
-      </div>
-      <div class="divider" />
-      <p id="desc-product">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sequi
-        dolor enim iusto tempora cumque mollitia obcaecati ducimus eum numquam
-        voluptate totam quos in. Deleniti explicabo quos aut quis eaque.
-      </p>
-      <div class="divider" />
-      <h1>$ini harga</h1>
-      <div class="button">
-        <div id="button-1">Buy Now</div>
-        <div id="button-2">Next Product</div>
-      </div>
+    <div v-else class="unavailable-product">
+      <p>This product is unavailable to show</p>
+      <button @click="getNextProduct">Next Product</button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      index: 1,
+      product: {},
+      productCategory: "men-section",
+      loading: false
+    };
+  },
+  methods: {
+    async fetchProduct() {
+      try {
+        this.loading = true;
+        const response = await fetch(
+          `https://fakestoreapi.com/products/${this.index}`
+        );
+        const data = await response.json();
+        if (
+          data.category === "men's clothing" ||
+          data.category === "women's clothing"
+        ) {
+          this.product = data;
+          this.productCategory =
+            data.category === "men's clothing"
+              ? "men-section"
+              : "women-section";
+        } else {
+          this.product = {};
+          this.productCategory = "unavailable-product";
+        }
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      } finally {
+        this.loading = false;
+      }
+    },
+    getNextProduct() {
+      this.index = this.index >= 20 ? 1 : this.index + 1;
+      this.fetchProduct();
+    },
+  },
+  mounted() {
+    this.fetchProduct();
+  },
+};
+</script>
 
 <style>
 .container {
@@ -42,6 +99,7 @@
   display: flex;
   flex: 1;
   flex-direction: column;
+  padding: 0 30px;
 }
 
 .left-section {
@@ -49,6 +107,11 @@
   justify-content: center;
   align-items: center;
   flex-basis: 33.33%;
+}
+
+.left-section img {
+  max-width: 100%;
+  max-height: 100%;
 }
 
 .right-section {
@@ -81,6 +144,55 @@
   display: flex;
   justify-content: space-between;
   gap: 20px;
+}
+
+.unavailable-product {
+  background-image: url('../assets/sad_face.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 400px;
+  width: 100%;
+}
+
+.unavailable-product p {
+  color: var(--dark-brown);
+  font-size: 30px;
+  text-align: center;
+}
+
+.unavailable-product button {
+  margin-top: 5px;
+  background: transparent;
+  border: 2px solid var(--dark-brown);
+  text-align: center;
+  padding: 10px 30px;
+  border-radius: 5px;
+  color: var(--dark-brown);
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.loader {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 8px solid #f3f3f3;
+  border-top: 8px solid var(--dark-pink); /* Blue */
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
 }
 
 h1 {
@@ -123,5 +235,3 @@ p {
   cursor: pointer;
 }
 </style>
-
-<script></script>
